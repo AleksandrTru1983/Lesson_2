e614c2ab8a86375e07d868cb42ca825bdcd59a3b
Aleksandr Trushin <atrushin1983@icloud.com>
2022-04-21 14:29:26 +0700

    Добавьте файлы проекта.

---
+++ Lesson_2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Enter your name: ");
+var name = Console.ReadLine();
+Console.WriteLine("*************************");
+Console.WriteLine($"Привет, {name}!");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Wellcome \nto\nWorld!");
+Console.WriteLine("*************************");
+Console.ReadLine();
